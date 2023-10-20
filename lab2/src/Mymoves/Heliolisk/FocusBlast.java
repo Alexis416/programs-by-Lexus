2e9beaf62c2c04106721ffdb9f3567883231b8da
Alexis416 <145903941+Alexis416@users.noreply.github.com>
2023-10-21 01:00:49 +0300

    Add files via upload

---
+++ lab2/src/Mymoves/Heliolisk/FocusBlast.java
@@ -0,0 +1,22 @@
+package Mymoves.Heliolisk;
+
+import Lab_2.Program;
+import ru.ifmo.se.pokemon.*;
+
+public class FocusBlast extends SpecialMove {
+    public FocusBlast(double pow, double acc){
+        super(Type.FIGHTING, pow, acc);
+    }
+    @Override
+    protected void applyOppEffects(Pokemon p){
+        super.applyOppEffects(p);
+        Effect def = new Effect().stat(Stat.SPECIAL_DEFENSE,-1);
+        if (Program.chance(0.1));
+        p.addEffect(def);
+    }
+
+    @Override
+    protected String describe(){
+        return "использует Focus Blast";
+    }
+}
