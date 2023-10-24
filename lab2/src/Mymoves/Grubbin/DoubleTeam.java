ec1e9c8763a1c8cdebba00b13a6a124a45a65c6b
Alexis416 <145903941+Alexis416@users.noreply.github.com>
2023-10-24 20:49:17 +0300

    Add files via upload

---
+++ lab2/src/Mymoves/Grubbin/DoubleTeam.java
@@ -0,0 +1,20 @@
+package Mymoves.Grubbin;
+
+import ru.ifmo.se.pokemon.*;
+
+public class DoubleTeam extends StatusMove {
+    public DoubleTeam(double pow, double acc){
+        super(Type.NORMAL, pow, acc);
+    }
+    @Override
+    protected void applySelfEffects(Pokemon p){
+        Effect evasion = new Effect().stat(Stat.EVASION,+1);
+        p.addEffect(evasion);
+    }
+
+    @Override
+    protected String describe(){
+        return "использует Double Team";
+    }
+
+}
