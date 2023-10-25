4d2a562870be3198d8a0260344a3a57852aea498
Alexis416 <145903941+Alexis416@users.noreply.github.com>
2023-10-25 12:24:43 +0300

    Add files via upload

--- lab2/src/Mypokemons/Heliolisk.java
+++ lab2/src/Mypokemons/Heliolisk.java
@@ -6,9 +6,9 @@ import ru.ifmo.se.pokemon.Type;
 public class Heliolisk extends Helioptile{
     public Heliolisk(String name, int level) {
         super(name, level);
-        super.setType(Type.ELECTRIC, Type.NORMAL);
-        super.setStats(62, 55, 52, 109, 94, 109);
+        setType(Type.ELECTRIC, Type.NORMAL);
+        setStats(62, 55, 52, 109, 94, 109);
         FocusBlast focusBlast = new FocusBlast(120, 70);
-        super.addMove(focusBlast);
+        addMove(focusBlast);
     }
 }
