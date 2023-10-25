aba763dcc572841c7d253524734b6f70223b3be6
Alexis416 <145903941+Alexis416@users.noreply.github.com>
2023-10-25 12:23:54 +0300

    Add files via upload

--- lab2/src/Mypokemons/Charjabug.java
+++ lab2/src/Mypokemons/Charjabug.java
@@ -6,8 +6,8 @@ import ru.ifmo.se.pokemon.Type;
 public class Charjabug extends Grubbin{
     public Charjabug(String name, int level) {
         super(name, level);
-        super.setType(Type.BUG, Type.ELECTRIC);
-        super.setStats(57, 82, 95, 55, 75, 36);
+        setType(Type.BUG, Type.ELECTRIC);
+        setStats(57, 82, 95, 55, 75, 36);
         Discharge discharge = new Discharge(80,100);
         addMove(discharge);
     }
