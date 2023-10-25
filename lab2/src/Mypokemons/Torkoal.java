9c1091698fb4efea61f9ee8063ec2447c21706a9
Alexis416 <145903941+Alexis416@users.noreply.github.com>
2023-10-25 12:25:35 +0300

    Add files via upload

--- lab2/src/Mypokemons/Torkoal.java
+++ lab2/src/Mypokemons/Torkoal.java
@@ -6,12 +6,12 @@ import ru.ifmo.se.pokemon.Type;
 public class Torkoal extends Pokemon {
     public Torkoal(String name, int level){
         super(name, level);
-        super.setType(Type.FIRE);
-        super.setStats(70, 85, 140, 85, 70, 20);
+        setType(Type.FIRE);
+        setStats(70, 85, 140, 85, 70, 20);
         RockSlide rockSlide = new RockSlide(75, 90);
         HeatWave heatWave = new HeatWave(95,90);
         Inferno inferno = new Inferno(100,50);
         StoneEdge stoneEdge = new StoneEdge(100,80);
-        super.setMove(rockSlide,heatWave,inferno,stoneEdge);
+        setMove(rockSlide,heatWave,inferno,stoneEdge);
     }
 }
