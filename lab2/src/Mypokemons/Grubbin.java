033c4f9243aa0bc87cda4aef7d2001f215d8c6f1
Alexis416 <145903941+Alexis416@users.noreply.github.com>
2023-10-25 12:24:22 +0300

    Add files via upload

--- lab2/src/Mypokemons/Grubbin.java
+++ lab2/src/Mypokemons/Grubbin.java
@@ -8,8 +8,8 @@ import ru.ifmo.se.pokemon.Type;
 public class Grubbin extends Pokemon {
     public Grubbin(String name, int level) {
         super(name, level);
-        super.setType(Type.BUG);
-        super.setStats(47, 62, 45, 55, 45, 46);
+        setType(Type.BUG);
+        setStats(47, 62, 45, 55, 45, 46);
         DoubleTeam doubleTeam = new DoubleTeam(0,0);
         ThunderWave thunderWave = new ThunderWave(0,90);
         setMove(doubleTeam,thunderWave);
