b2d26e847666226163d0ebc7178463fd73489310
Alexis416 <145903941+Alexis416@users.noreply.github.com>
2023-11-07 22:13:30 +0300

    Add files via upload

--- lab2/src/Mymoves/Helioptile/WildCharge.java
+++ lab2/src/Mymoves/Helioptile/WildCharge.java
@@ -1,24 +1,16 @@
 package Mymoves.Helioptile;
 
 import ru.ifmo.se.pokemon.PhysicalMove;
-import ru.ifmo.se.pokemon.Pokemon;
 import ru.ifmo.se.pokemon.Type;
 
 public class WildCharge extends PhysicalMove {
     public WildCharge(double pow, double acc){
         super(Type.ELECTRIC, pow, acc);
     }
-    @Override
-    protected void applySelfDamage(Pokemon def, double damage){
-        super.applySelfDamage(def, damage);
-    }
-    @Override
-    protected double calcBaseDamage(Pokemon att, Pokemon def){
-        return 1d/4d;
-    }
 
     @Override
     protected String describe(){
         return "использует Wild Charge";
     }
+
 }
