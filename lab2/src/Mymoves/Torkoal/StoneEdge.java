e6e6ca4270186583d887ac84a06ef64071193a18
Alexis416 <145903941+Alexis416@users.noreply.github.com>
2023-11-07 20:41:04 +0300

    Add files via upload

--- lab2/src/Mymoves/Torkoal/StoneEdge.java
+++ lab2/src/Mymoves/Torkoal/StoneEdge.java
@@ -9,10 +9,7 @@ public class StoneEdge extends PhysicalMove {
     public StoneEdge(double pow, double acc){
         super(Type.ROCK, pow, acc);
     }
-    @Override
-    protected void applyOppDamage(Pokemon def, double damage){
-        super.applyOppDamage(def, damage);
-    }
+
     @Override
     protected double calcCriticalHit(Pokemon att, Pokemon def){
         if (att.getStat(Stat.SPEED) / (512.0 / 3) > Math.random()){
