386894975c91fc054d818c21c916c9514fbf1875
Alexis416 <145903941+Alexis416@users.noreply.github.com>
2023-12-21 20:47:26 +0300

    Add files via upload

---
+++ lab4/src/enums/Action.java
@@ -0,0 +1,8 @@
+package enums;
+
+public enum Action {
+    said,said_what,feels,needs_to_be_sent_immediately,can_get_free,by_giving_an_order,
+    do_not_go_out,so_as_not_to_be_exposed,went,was_received,were_wearing,ordered_to_get_attached,
+    was_prepared,completed,set,attached,fell_out,tied_up,were_finished,lined_up,turned_on,
+    was_attached,pressed, heard,spun,got_up,dragged,gasped_in_amazement,screamed,jumped,cried
+}
